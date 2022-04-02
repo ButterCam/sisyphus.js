8a2857d240e37c607c531d41d9bd9ab743bf0f11
Kanro <higan@live.cn>
2022-04-02 17:59:52 +0800

    🔀 :bookmark: Release 0.0.5 (#143)
    * :bug: Fix any field
    
    * :bookmark: Release 0.0.5

--- packages/sisyphus-core/src/wellknown/empty.ts
+++ packages/sisyphus-core/src/wellknown/empty.ts
@@ -1,7 +1,20 @@
-import {Message} from "../message"
+import {JsonValue, Message} from '../message'
 
 export interface IEmpty {
 }
 
 export class Empty extends Message<Empty> implements IEmpty {
+    static readonly INSTANCE: Readonly<Empty> = new Empty()
+
+    static create(properties: Empty | IEmpty | undefined | null): Empty {
+        return Empty.INSTANCE
+    }
+
+    static fromJson(object: JsonValue): Empty {
+        return Empty.INSTANCE
+    }
+
+    static toJson(message: Empty | IEmpty | undefined | null): JsonValue {
+        return {}
+    }
 }
