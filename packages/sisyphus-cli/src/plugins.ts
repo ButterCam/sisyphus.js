7258dd0dbf93921da2dc659a82f7cb32516eef93
higan <higan@live.cn>
2022-06-29 14:40:47 +0800

    :bug: Skip plugins discover for deep scanning

--- packages/sisyphus-cli/src/plugins.ts
+++ packages/sisyphus-cli/src/plugins.ts
@@ -28,10 +28,9 @@ export async function discoverPluginsInModule(packageInfo: PackageJson, paths: s
                 queue.push(dep)
                 continue
             }
-            if(info.main !== undefined) {
+            if (info.main !== undefined) {
                 require(modulePath)
             }
-            await discoverPluginsInModule(info, [path.join(modulePath, 'node_modules')].concat(paths))
         }
         devDeps = queue
         queue = []
